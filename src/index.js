1e744af66066820ceb8eb084fbdc9aa52daaa487
oswaldosalazar <oswaldo.r.salazar@gmail.com>
2017-04-19 23:34:07 -0600

    Imported main libraries to render elements

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// Create a new component. This componenet should produce some HTML
+
+const App = () => {
+  return <div>Hi!</div>;
+}
+
+// Take this components' generated HTML amd put it on the page (in the DOM)
+
+ReactDOM.render(<App />, document.querySelector('.container')); 
