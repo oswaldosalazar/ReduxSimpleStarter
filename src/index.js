fe02c14ee0458e8811339da94fdb86fd97554b7a
oswaldosalazar <oswaldo.r.salazar@gmail.com>
2017-04-21 17:29:11 -0600

    Test API key and YTSearch functionality

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/search_bar';
 
 const API_KEY = 'AIzaSyByf-3h1ckVCxriNH5aQaERzqpYZhudBV0';
+
+YTSearch({key: API_KEY, term:'Piazzolla'}, data => console.log(data))
 
 // Create a new component. This componenet should produce some HTML
 
